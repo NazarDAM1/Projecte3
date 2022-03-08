61f990e8f8f553d9032980435f11908c30a4e98b
NazarDAM1 <94818024+NazarDAM1@users.noreply.github.com>
2022-03-08 08:30:45 +0100

    Delete bin directory

--- bin/2.sql
+++
@@ -1,5 +0,0 @@
-CREATE DEFINER=`root`@`localhost` PROCEDURE `consultaClientsMesReserves`()
-BEGIN
-SELECT clients.*,count(registrencolect.dni) + (Select count(registrenindv.dni) from registrenindv where registrenindv.dni = clients.dni)as _suma FROM registrencolect, clients where registrencolect.dni = clients.dni group by registrencolect.dni order by count(registrencolect.dni) desc;
-
-END
